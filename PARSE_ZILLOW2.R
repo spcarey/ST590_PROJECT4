4af220cf2661d539a02945897330ab3ab2b57c83
Sean carey <scarey9@gmail.com>
2018-12-14 14:25:55 -0500

    *moving data into rmd *first model done *begin Parallel

--- PARSE_ZILLOW2.R
+++ PARSE_ZILLOW2.R
@@ -1,6 +1,6 @@
 
 
-Parse_Zillow_2 <- function(x){
+Parse_Zillow <- function(x){
    
    X <- setNames(as.data.frame(matrix(ncol = 12, nrow = 1)), c("street","zip", "city","Use_Code", "Tax_Year", "Tax_Value","year_built", "lot_size", "finished_sq_feet", "bathrooms","bedrooms", "zestimate"))
    
@@ -31,30 +31,9 @@ Parse_Zillow_2 <- function(x){
   return(X)
 }
 
-DF[1,1] <- xmlValue(x[["//street"]])
-DF[1,2] <- xmlValue(x[["//zipcode"]])
-DF[1,3] <- xmlValue(x[["//city"]])
-DF[1,4] <- xmlValue(x[["//useCode"]])
-DF[1,5] <- xmlValue(x[["//taxAssessmentYear"]])
-DF[1,6] <- xmlValue(x[["//taxAssessment"]])
-DF[1,7] <- xmlValue((x[["//yearBuilt"]]))
-DF[1,8] <- xmlValue(x[["//lotSizeSqFt"]])
-DF[1,9] <- xmlValue(x[["//finishedSqFt"]])
-DF[1,10] <- xmlValue(x[["//bathrooms"]])
-DF[1,11] <- xmlValue(x[["//bedrooms"]]) 
-DF[1,12] <- xmlValue(x[["//amount"]])
-DF[1,13] <- "null"
-DF[1,14] <- "null"
 
-LA_SMALL <- LA_SAMPLE_VECTOR[1:5, ]
 
-XX <- Parse_Zillow_2(LA_SMALL)
-
-XX <- lapply(as.list(LA_SMALL), Parse_Zillow_2)
-
-TEST_FRAME <- apply(as.data.frame(LA_SMALL) ,MARGIN = 1,Parse_Zillow_2 )
-
-TEST_FRAME <- map_df(LA_SAMPLE_VECTOR[1:999, ],  Parse_Zillow_2)
+TEST_FRAME <-    map_df(LA_SAMPLE_VECTOR[1:999, ],  Parse_Zillow_2)
 TEST_FRAME_2 <-  map_df(LA_SAMPLE_VECTOR[1000:1999, ],  Parse_Zillow_2)
 Test_FRAME_3 <-  map_df(LA_SAMPLE_VECTOR[2000:2999,],  Parse_Zillow_2)
 
@@ -63,9 +42,10 @@ TEST_FRAME_5 <-  map_df(LA_SAMPLE_VECTOR[4000:4950, ],  Parse_Zillow_2)
 
 
 
-TEST_FRAME <- TEST_FRAME %>% filter(!is.na(zestimate))
+TEST_FRAME <- TEST_FRAME %>% filter(!is.na(zestimate)) %>% as.tibble()
 
-TEST_FRAME[504, ]
+TEST_FRAME
 
 ZILLOW_XML
 
+NoMISS <- MODEL_TRAIN %>% na.omit()
